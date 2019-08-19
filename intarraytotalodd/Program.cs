c3548ca6e464495208e17cf6f47a740a398a1af1
Kristen Shea <kstar61@live.com>
2019-08-18 21:57:18 -0500

    /

--- intarraytotalodd/Program.cs
+++ intarraytotalodd/Program.cs
@@ -2,11 +2,27 @@
 
 namespace intarraytotalodd
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //add odd numbers only from an array
+
+            int[] numbers = { 0, 14, 12, 22, 23, 18, 11, 4, 2, 3, 8 };
+
+            int total = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 1)
+                {
+                    total = total + number;
+                }
+            }
+
+            Console.WriteLine($"{total}");
+
+            Console.ReadLine();
         }
     }
 }
